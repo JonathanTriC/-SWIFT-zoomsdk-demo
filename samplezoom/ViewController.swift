d8db57d09b2c0f6bfeb773944c01a8b9e4b21d75
Jonathan Tri <jonathantric@gmail.com>
2022-05-10 09:03:30 +0700

    Initial Commit

---
+++ samplezoom/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  samplezoom
+//
+//  Created by Jonathan Tri Christianto on 10/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
